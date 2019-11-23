23dc54bdfb2d367818366c58e0fb5bc837b4e7e9
welldo-mac <wangduo@cechealth.cn>
2019-11-23 20:40:21 +0800

    构造器

---
+++ src/main/scala/com/welldo/scalaStudy/chapter6/constructor3/ConstructDemo4.scala
@@ -0,0 +1,29 @@
+package com.welldo.scalaStudy.chapter6.constructor3
+
+import scala.beans.BeanProperty
+
+/**
+  *JavaBeans规范定义了Java的属性是像getXxx（）和setXxx（）的方法。
+  * 许多Java工具（框架）都依赖这个命名习惯。
+  * 将Scala字段加@BeanProperty时，这样会自动生成规范的 setXxx/getXxx 方法。
+  * 这时可以使用 对象.setXxx() 和 对象.getXxx() 来调用属性。
+  *
+  * author: welldo
+  * date: 2019/11/23 19:21
+  */
+object ConstructDemo4 {
+  def main(args: Array[String]): Unit = {
+    val car = new Car
+    car.name = "奔驰" //底层是 car.name.$eq()
+    println(car.name) //底层使用的是car.name()
+
+    car.setName("路虎")
+    println(car.getName)
+
+  }
+}
+
+class Car{
+  @BeanProperty var name:String = _
+}
+
