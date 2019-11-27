17c884568d737f79685f55910bf3e1d7189463af
welldo-mac <wangduo@cechealth.cn>
2019-11-27 15:07:12 +0800

    方法调用机制

---
+++ src/main/scala/com/welldo/scalaStudy/chapter5/function1/Function2.scala
@@ -0,0 +1,27 @@
+package com.welldo.scalaStudy.chapter5.function1
+
+/*
+  * *****方法的调用机制
+  * 详细请看示意图  方法的调用机制.png
+  *
+  * 1.当程序执行一个函数/方法时, 会开辟一个独立的栈(即相互的数据独立)
+  * 2.当遇到return或者执行完成, 会返回到调用的位置
+  *
+  * author: welldo 
+  * date: 2019/11/17 19:26
+  */
+object Function2 {
+
+  def main(args: Array[String]): Unit = {
+    val n1 = 1
+    val n2 = 3
+    val res = sum(n1, n2)
+    println("res=" + res)
+  }
+
+  def sum(n1: Int, n2: Int): Int = {
+    return n1 + n2
+  }
+
+
+}
