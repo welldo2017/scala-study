3e5b403ccc35a9069807f0a28186a7cc23f84b16
welldo-win <welldo2012-win@qq.com>
2019-12-18 23:17:31 +0800

    map flatmap filter...

--- src/main/scala/com/welldo/scalaStudy/chapter11/HighOrderFunction3.scala
+++ src/main/scala/com/welldo/scalaStudy/chapter11/HighOrderFunction3.scala
@@ -5,10 +5,7 @@ package com.welldo.scalaStudy.chapter11
   * 将其结果放到一个新的集合中返回，即返回一个新的List(6,10,14),
   *
   * map()的定义: def map[B, That](f: A => B)
-  * map()的参数是:  f:int => B 的函数(B 是泛型)
-  * 所以我们需要 手动写一个函数, 接收int参数, 返回值为泛型B, 也就是任意
-  *
-  * 170 结束
+  * map()的参数是: 接受一个值,返回一个值的函数 即可
   *
   * author welldo
   * date 2019/12/17
@@ -21,14 +18,15 @@ object HighOrderFunction3 {
 
     //map() 做了什么事儿
     //1. 将list中的元素, 依次遍历
-    //2. 依次放入到 muliple() 函数中, 并返回一个int (就是说, 有几个元素就调用几次muliple)
-    //3. 将得到的int, 依次放入到新的list 中
+    //2. 依次放入到 muliple() 函数中, 运算后返回一个值 (就是说, 有几个元素就调用几次muliple)
+    //3. 将运算得到的值, 依次放入到新的list 中,
+    //4. 最终返回新的list
     val newList = list.map(muliple _)
     println(newList)
 
   }
 
-  //f:int => B
+  //接受一个值,返回一个值的函数 即可; 这里我们定义为接收int,返回int
   def muliple(n:Int)={
     2*n
   }
